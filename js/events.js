848e28037b2a319ab1f05703dd243d9434c1d651
l3eto <beto_2613@hotmail.com>
2016-04-25 21:43:36 +0200

    beru13

--- js/events.js
+++ js/events.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
-  
+
+  $('#rev-23-04-16').find('.ui.circular.facebook.icon.button').click( function(){
+      var win = window.open( 'https://www.facebook.com/revolutionc29/photos/a.1424212441144767.1073741828.1424015387831139/1766580310241310/', '_blank');
+      win.focus();
+  });
+
+  $('#rev-23-04-16').find('.ui.circular.instagram.icon.button').click( function(){
+      var win = window.open( 'https://www.instagram.com/p/BEcA0r4SkLW/', '_blank');
+      win.focus();
+  });
+
 });
