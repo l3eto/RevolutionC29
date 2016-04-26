e7dd5c0478a941d7d3bee09c7bfc91ac8495f739
Beto <beto_2613@hotmail.com>
2016-04-26 11:49:01 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -5,7 +5,17 @@ $( document ).ready(function() {
   $('.ui.huge.black.button.home').click( function(){
       window.location.href = '../events/';
   });
-
+  
+  document.getElementById("bgvid").addEventListener('ended', function(){
+  // only functional if "loop" is removed 
+  this.pause();
+    // to capture IE10
+  vidFade( this );
+  });
+  
+  function vidFade( vid ) {
+    vid.classList.add("stopfade");
+  }
   //song_home = new Audio('../audio/Hillsong Young & Free - Wake.mp3');
   //song_home.play();
 
