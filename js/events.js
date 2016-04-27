64917ba5a328e9a57f6322af84e6d272bbb35fce
l3eto <beto_2613@hotmail.com>
2016-04-27 18:45:47 +0200

    berulin02

--- js/events.js
+++ js/events.js
@@ -1,5 +1,15 @@
 $( document ).ready(function() {
 
+  $('#rev-30-04-16').find('.ui.circular.facebook.icon.button').click( function(){
+      var win = window.open( 'https://www.facebook.com/revolutionc29/photos/a.1424212441144767.1073741828.1424015387831139/1769313223301352/', '_blank');
+      win.focus();
+  });
+
+  $('#rev-30-04-16').find('.ui.circular.instagram.icon.button').click( function(){
+      var win = window.open( 'https://www.instagram.com/p/BEtfBrVykHI/', '_blank');
+      win.focus();
+  });
+
   $('#rev-23-04-16').find('.ui.circular.facebook.icon.button').click( function(){
       var win = window.open( 'https://www.facebook.com/revolutionc29/photos/a.1424212441144767.1073741828.1424015387831139/1766580310241310/', '_blank');
       win.focus();
