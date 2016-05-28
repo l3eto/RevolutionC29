a45defe85383e2dd5cb5aacdf6879f475936b811
l3eto <beto_2613@hotmail.com>
2016-05-28 14:18:35 +0200

    beru

--- js/events.js
+++ js/events.js
@@ -1,10 +1,36 @@
 $( document ).ready(function() {
 
+  $('#rev-28-05-16').find('.ui.circular.facebook.icon.button').click( function(){
+      var win = window.open( 'https://www.facebook.com/revolutionc29/photos/a.1424212441144767.1073741828.1424015387831139/1780890342143640/', '_blank');
+      win.focus();
+  });
+  $('#rev-28-05-16').find('.ui.circular.instagram.icon.button').click( function(){
+      var win = window.open( 'https://www.instagram.com/p/BF81RsNykOn/', '_blank');
+      win.focus();
+  });
+
+  $('#rev-21-05-16').find('.ui.circular.facebook.icon.button').click( function(){
+      var win = window.open( 'https://www.facebook.com/revolutionc29/photos/a.1424212441144767.1073741828.1424015387831139/1778284352404239/', '_blank');
+      win.focus();
+  });
+  $('#rev-21-05-16').find('.ui.circular.instagram.icon.button').click( function(){
+      var win = window.open( 'https://www.instagram.com/p/BFqfwf8SkEF/', '_blank');
+      win.focus();
+  });
+
+  $('#rev-07-05-16').find('.ui.circular.facebook.icon.button').click( function(){
+      var win = window.open( 'https://www.facebook.com/revolutionc29/photos/a.1424212441144767.1073741828.1424015387831139/1773279912904683/', '_blank');
+      win.focus();
+  });
+  $('#rev-07-05-16').find('.ui.circular.instagram.icon.button').click( function(){
+      var win = window.open( 'https://www.instagram.com/p/BFG0SegykI-/', '_blank');
+      win.focus();
+  });
+
   $('#rev-30-04-16').find('.ui.circular.facebook.icon.button').click( function(){
       var win = window.open( 'https://www.facebook.com/revolutionc29/photos/a.1424212441144767.1073741828.1424015387831139/1769313223301352/', '_blank');
       win.focus();
   });
-
   $('#rev-30-04-16').find('.ui.circular.instagram.icon.button').click( function(){
       var win = window.open( 'https://www.instagram.com/p/BEtfBrVykHI/', '_blank');
       win.focus();
@@ -14,7 +40,6 @@ $( document ).ready(function() {
       var win = window.open( 'https://www.facebook.com/revolutionc29/photos/a.1424212441144767.1073741828.1424015387831139/1766580310241310/', '_blank');
       win.focus();
   });
-
   $('#rev-23-04-16').find('.ui.circular.instagram.icon.button').click( function(){
       var win = window.open( 'https://www.instagram.com/p/BEcA0r4SkLW/', '_blank');
       win.focus();
