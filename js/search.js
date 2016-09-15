08816b4e9e71d23927e7fbdb3577a04bd2b81026
Beto <beto_2613@hotmail.com>
2016-09-15 19:16:03 +0200

    Create search.js

---
+++ js/search.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+  
+  //read xml
+  function readXML(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/xml");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+  }
+  readXML("../uploads/songs.xml", function(data){
+    if (window.DOMParser){
+      parser = new DOMParser();
+      xmlDoc = parser.parseFromString(data, "text/xml");
+    }else{
+      xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+      xmlDoc.async = false;
+      xmlDoc.loadXML(data);
+    }
+  
+  var songs = xmlDoc.getElementsByTagName("event");
+  console.log(songs);
+  if( events.length > 0 ){
+    for (var i = 0; i < events.length; i++) {
+    
+    }
+  }
+    
+  });
+  
+
+});
