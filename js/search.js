260c6aefe5560d6ac41d8cdf90fd25598519857b
Beto <beto_2613@hotmail.com>
2016-09-16 21:43:26 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -197,8 +197,6 @@ function setLetras( song , orden ){
   var songcontainer = document.createElement('DIV');
   songcontainer.setAttribute("class","ui song-container");
   for( var i = 0 ; i < orden.length ; i++ ){
-    console.log( orden );
-    console.log( $( song ).find('lyric[name="'+orden[i]+'"]').get(0) );
     var lyric = $( song ).find('lyric[name="'+orden[i]+'"]').get(0).getElementsByTagName("p");
     if( lyric.length > 0 ){
       for( var j = 0 ; j < lyric.length ; j++ ){
