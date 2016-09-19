6fdca6737d10ad777375cd82c2eb41be095eb7c8
Beto <beto_2613@hotmail.com>
2016-09-19 17:43:06 +0200

    Update notes.js

--- js/notes.js
+++ js/notes.js
@@ -1,21 +1,26 @@
-var upnote = function( note , tone ){
-  var note = new note( note );
-  note.up( tone );
+var modifynote = function( element , tone ){
+  $('.beru-note').each(function( index ) {
+    console.log( index + ": " + $( this ).text() );
+  });
 }
+
+
+var Note = function( note ){
+  this._mayors = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#'];
+  this._minors = ['Am','A#m','Bm','Cm','C#m','Dm','D#m','Em','Fm','F#m','Gm','G#m'];
+  if( this._mayors.indexOf( note ) > -1 ){
+    var position = this._mayors.indexOf( note );
+    console.log(this);
+  }else if( this._minors.indexOf( note ) > -1 ){
+    var pos = this._minorss.indexOf( note );
+    console.log(this);
+  }else{
+    console.log('cant up this note: '+note);
+  }
+}
+
 
 var shownote = function( element ){
   $('.ui.modal.note').find('.ui.center.aligned.header').html( $(element).html() );
   $('.ui.modal.note').modal({blurring: true}).modal('show').modal('setting', 'transition', 'scale');
-}
-
-var note = function(){
-  this.notes = [
-    'Am','A','A#',
-    'Bm','B',
-    'Cm','C','C#',
-    'Dm','D','D#',
-    'Em','E',
-    'Em','F','F#',
-    'Gm','G','G#'
-  ]
 }
