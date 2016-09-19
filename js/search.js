0c332117e6e7c93483c887f7d8ef7dd0f205e7c1
Beto <beto_2613@hotmail.com>
2016-09-19 15:18:24 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -121,7 +121,7 @@ $( document ).ready(function(){
       var songName = song.getAttribute("name");
       var title = artistName+' - '+songName;
       if( artists[0].getAttribute("src") ){ var imgsrc = "../uploads/img/".concat(artists[0].getAttribute("src"),".jpg");}
-      else{ var imgsrc = "../uploads/img/unknow.jpg");}
+      else{ var imgsrc = "../uploads/img/unknow.jpg";};
       var orden = song.getAttribute("order").split("-");
       document.title = songName+' | Revolution C29';
       audio = new Audio( '../audio/'+encodeURI(title)+'.mp3');
