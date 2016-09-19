f416a6fd08b8946dcae041b53477e3ac3726cce0
Beto <beto_2613@hotmail.com>
2016-09-19 15:36:06 +0200

    Create notes.js

---
+++ js/notes.js
@@ -0,0 +1,12 @@
+var upnote = function( note , tone ){
+  var note = new note( note );
+  note.up( tone );
+}
+
+var note = function(){
+ this.A = 
+ this.A = 
+ this.A = 
+ this.A = 
+ this.A = 
+}
