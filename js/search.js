892b5a773b0ea97dac45a5446d9fcfdf67ccf247
Beto <beto_2613@hotmail.com>
2016-09-21 17:28:07 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -163,13 +163,7 @@ $( document ).ready(function(){
       }
       //get artist
       var artist = $( xmlDoc ).find('artist[id="'+searchingartist+'"]');
-      var artists = song.getElementsByTagName("artist");
-      var artist = [];
-      for( var j = 0 ; j < artists.length ; j++ ){
-        artist.push( artists[j].getAttribute("name") );
-      }
-      var artistName = artist.join(" & ");
-      //var artistName = artist.get(0).getAttribute("name");
+      var artistName = artist.get(0).getAttribute("name");
       //playlist 
       playlist = new Playlist(artist);
       //set title
