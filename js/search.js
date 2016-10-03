35fcdee0b28411e515f1f135ecd5f10bdf9699f5
Beto <beto_2613@hotmail.com>
2016-10-03 19:28:32 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -159,7 +159,7 @@ $( document ).ready(function(){
       }
       //get 5 latest songs
       var songs = $( xmlDoc ).find('song');
-      setTitleNoSearching("thumbs up","5 últimas Actualizaciones");
+      setTitleNoSearching("thumbs up","5 Últimas Actualizaciones");
       setDivition();
       setLastSongs( songs , 5 );
     });
